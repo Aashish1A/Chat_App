eb0d703191227bdc4668ab31844c23e9d70cd3fa
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-10-21 01:09:07 +0530

    refactor(auth): Refactor code

--- Server/middleware/auth.js
+++ Server/middleware/auth.js
@@ -6,9 +6,13 @@ import jwt from "jsonwebtoken";
 export const protectRoute = async (req, res, next) => {
     try {
         const token = req.headers.token;
+
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
         const user = await User.findById(decoded.userId).select("-password");
+
         if (!user) return res.status(404).json({ message: "User not found" });
+        
         req.user = user;
         next();
     } catch (error) {
