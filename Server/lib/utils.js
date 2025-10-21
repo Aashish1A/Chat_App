cca36d2973dbaabafa6c9bb78e74574f9014fe64
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-10-22 00:12:56 +0530

    feat(utils): Remove jwt expires and refactor formatMessageTime function

--- Server/lib/utils.js
+++ Server/lib/utils.js
@@ -2,6 +2,6 @@ import jwt from 'jsonwebtoken';
 
 // Function to generate a JWT token for user
 export const generateToken = (userId) => {
-    const token = jwt.sign({userId}, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const token = jwt.sign({userId}, process.env.JWT_SECRET);
     return token;
 };
