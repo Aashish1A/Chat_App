a50bf64e184844ff6d19722da413f2abbc2a3442
Aashish Kumar <166286708+Aashish1A@users.noreply.github.com>
2025-10-22 00:14:16 +0530

    refactor(userController): Added true and false for response status and fixed minor issues

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -9,13 +9,13 @@ export const signup = async (req, res) => {
         const { fullName, email, password, bio } = req.body;
 
         if(!fullName || !email || !password || !bio) {
-            return res.status(400).json({ message: "All fields are required" });
+            return res.status(400).json({ success: false, message: "All fields are required" });
         }
 
         // Check if user already exists
         const existingUser = await User.findOne({ email });
         if (existingUser) {
-            return res.status(400).json({ message: "User already exists" });
+            return res.status(400).json({ success: false, message: "User already exists" });
         }
 
         const salt = await bcrypt.genSalt(10);
@@ -23,16 +23,14 @@ export const signup = async (req, res) => {
 
         // Create new user
         const newUser = new User({ fullName, email, password: hashedPassword, bio });
-
-        const token = generateToken(newUser._id);
-        newUser.token = token;
-
         await newUser.save();
 
-        res.status(201).json({ message: "Account created successfully", user: newUser, token });
+        const token = generateToken(newUser._id);
+
+        res.status(201).json({success: true, message: "Account created successfully", user: newUser, token });
     } catch (error) {
         console.error("Error creating user:", error);
-        res.status(500).json({ message: "Internal server error" });
+        res.status(500).json({ success: false, message: "Internal server error" });
     }
 }
 
@@ -42,30 +40,27 @@ export const login = async (req, res) => {
         const { email, password } = req.body;
 
         if (!email || !password) {
-            return res.status(400).json({ message: "All fields are required" });
+            return res.status(400).json({ success: false, message: "All fields are required" });
         }
 
         // Check if user exists
         const user = await User.findOne({ email });
         if (!user) {
-            return res.status(400).json({ message: "Invalid credentials" });
+            return res.status(400).json({ success: false, message: "Invalid credentials" });
         }
 
         // Check password
         const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
-            return res.status(400).json({ message: "Invalid credentials" });
+            return res.status(400).json({ success: false, message: "Invalid credentials" });
         }
 
         const token = generateToken(user._id);
-        user.token = token;
 
-        await user.save();
-
-        res.status(200).json({ message: "Login successful", user, token });
+        res.status(200).json({success: true, message: "Login successful", user, token });
     } catch (error) {
         console.error("Error logging in user:", error);
-        res.status(500).json({ message: "Internal server error" });
+        res.status(500).json({ success: false, message: "Internal server error" });
     }
 }
 
@@ -77,21 +72,21 @@ export const checkAuth = async (req, res) => {
 // Controller to update user profile details
 export const updateProfile = async (req, res) => {
     try {
-        const {profilePic, bio, fullName} = req.body;
+        const {profilePicture, bio, fullName} = req.body;
 
         const userId = req.user._id;
         let updatedUser;
 
-        if(!profilePic){
+        if(!profilePicture){
            updatedUser = await User.findByIdAndUpdate(userId, {bio, fullName}, {new: true});
         }else{
-            const upload = await cloudinary.uploader.upload(profilePic);
-            updatedUser = await User.findByIdAndUpdate(userId, {bio, fullName, profilePic: upload.secure_url}, {new: true});
+            const upload = await cloudinary.uploader.upload(profilePicture);
+            updatedUser = await User.findByIdAndUpdate(userId, {bio, fullName, profilePicture: upload.secure_url}, {new: true});
         }
 
-        res.status(200).json({ message: "Profile updated successfully", user: updatedUser });
+        res.status(200).json({ success: true, message: "Profile updated successfully", user: updatedUser });
     } catch (error) {
         console.error("Error updating user profile:", error);
-        res.status(500).json({ message: "Internal server error" });
+        res.status(500).json({ success: false, message: "Internal server error" });
     }
 }
